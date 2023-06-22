6ebd82e7d48e66ecb92e474c974d99cb69a0378f
Irshad Khan <webdev727@gmail.com>
2023-06-22 21:13:41 +0530

    Edit user

--- src/app/User Management Component/user-management/user-management.component.ts
+++ src/app/User Management Component/user-management/user-management.component.ts
@@ -7,17 +7,39 @@ import { Component } from '@angular/core';
 })
 export class UserManagementComponent {
    users:string[] = [];
-   newUser:string = ''
+   newUser:string = '';
+   selectedUser:string = '';
+   selectedUserIndex: number = -1;
 
 
    addUser():void{
-     if(this.newUser.trim() !== ''){
-      this.users.push(this.newUser);
-      this.newUser = '';
-     }
+    if (this.selectedUser.trim() !== '') {
+      this.users.push(this.selectedUser);
+      this.selectedUser = '';
+    }
+   }
+
+   editUser(index:number):void{
+    this.selectedUser = this.users[index];
+    this.selectedUserIndex = index;
+   }
+
+   updateUser(){
+    console.log('Updating user:', this.selectedUser);
+    if (this.selectedUserIndex !== -1 && this.selectedUser.trim() !== '') {
+      this.users[this.selectedUserIndex] = this.selectedUser;
+      this.selectedUser = '';
+      this.selectedUserIndex = -1;
+    }
+    console.log('Updated users:', this.users);
    }
  
    deleteUser(i: number){
     this.users.splice(i,1);
    }
+
+   cancelEdit(): void {
+    this.selectedUser = '';
+    this.selectedUserIndex = -1;
+  }
 }
